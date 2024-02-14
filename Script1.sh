1a91f4769b1b38d04016f9e11c73e9c6113d271a
shreyas <shreyasakade9@gmail.com>
2024-02-14 18:28:25 +0530

    this real code

--- Script1.sh
+++ Script1.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-#This is my 1st shell script to print output
+<?xml version="1.0" encoding="UTF-8"?>
+<classpath>
+	<classpathentry kind="src" path="src"/>
+	<classpathentry kind="con" path="org.eclipse.jdt.launching.JRE_CONTAINER/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType/JavaSE-1.8"/>
+	<classpathentry kind="output" path="bin"/>
+</classpath>
